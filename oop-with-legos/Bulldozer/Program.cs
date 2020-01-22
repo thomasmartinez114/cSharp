df215eb1fd8e20c8f40015026dd240dbdecca35d
tmartinez114 <tmartinez114@gmail.com>
2020-01-21 19:32:15 -0500

    created Bulldozer class to print: Bulldozer is: yellow

---
+++ oop-with-legos/Bulldozer/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Bulldozer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var dozer = new BullDozer();
+            dozer.color = "Yellow";
+            dozer.size = "Mini";
+
+            Console.WriteLine("Bulldozer is: " + dozer.color);
+        }
+    }
+
+    public class BullDozer
+    {
+        public string color { get; set; }
+        public string size { get; set; }
+    }
+}
