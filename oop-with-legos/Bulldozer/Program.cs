f0a054d700632d1b0c6692bbb4d50987156e1d43
tmartinez114 <tmartinez114@gmail.com>
2020-01-30 19:52:04 -0500

    added a Constructor to utilize and save time when making multiple Bulldozer objects - dont have to type the color and size variables

--- oop-with-legos/Bulldozer/Program.cs
+++ oop-with-legos/Bulldozer/Program.cs
@@ -6,16 +6,24 @@ namespace Bulldozer
     {
         static void Main(string[] args)
         {
-            var dozer = new BullDozer();
-            dozer.color = "Yellow";
-            dozer.size = "Mini";
+            var dozer = new BullDozer("Yellow", "Mini");
 
-            Console.WriteLine("Bulldozer is: " + dozer.color);
+            var dozer2 = new BullDozer("Red", "Large");
+
+            Console.WriteLine("Bulldozer 1 is: " + dozer.color);
+            Console.WriteLine("Bulldozer 2 is: " + dozer2.color);
         }
     }
 
     public class BullDozer
     {
+        // Constructor
+        public BullDozer(string _color, string _size)
+        {
+            this.color = _color;
+            this.size = _size;
+        }
+
         public string color { get; set; }
         public string size { get; set; }
     }
