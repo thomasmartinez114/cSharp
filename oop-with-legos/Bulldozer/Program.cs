fc0fdcad8e6d17611399cce1e8f2ce739d713048
tmartinez114 <tmartinez114@gmail.com>
2020-02-01 18:29:27 -0500

    Polymorphism - having methods to run only on specifics that call it

--- oop-with-legos/Bulldozer/Program.cs
+++ oop-with-legos/Bulldozer/Program.cs
@@ -9,6 +9,8 @@ namespace Bulldozer
             var worker = new FactoryWorker();
             worker.Count(4);
             worker.Count(5);
+
+            worker.Count(4.5f);
         }
     }
 
@@ -18,14 +20,24 @@ namespace Bulldozer
         {
 
         }
+
+        // Only called when a int is in
         public void Count(int number)
         {
             if (Total == null)
                 Total = 0;
 
+            Total = Total + Convert.ToSingle(number);
+            Console.WriteLine(Total.ToString());
+        }
+
+        // Only called when a float is in
+        public void Count(float number)
+        {
             Total = Total + number;
             Console.WriteLine(Total.ToString());
         }
-        public int Total { get; set; }
+
+        public float Total { get; set; }
     }
 }
